fd1464ec92e91a26cbb6b9c29c1b9b082b02f6d1
Markbaldera <58714695+johnmarkbalderra@users.noreply.github.com>
2024-12-16 22:16:15 +0800

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "land";
+$servername = "fdb1029.awardspace.net";
+$username = "4564847_land";
+$password = "landlto123";
+$dbname = "4564847_land";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -10,5 +10,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully!";
 }
 ?>
